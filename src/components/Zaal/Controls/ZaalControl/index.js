7d083cf799c3e8da38e30c4be5cd67166a0803aa
Zzzokki <65149963+Zzzokki@users.noreply.github.com>
2021-03-01 21:54:15 +0800

    utasniih ni bologu bga, ter taguud garaad bsnig zassan style dotorh styliig shinechlehed bolno, uur yum bval hleerei

--- src/components/Zaal/Controls/ZaalControl/index.js
+++ src/components/Zaal/Controls/ZaalControl/index.js
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import config from "../../../../config/config.json";
 import styles from "../Style/_.module.css";
-import SidebarSearch from "../SidebarSearch";
 import Loader from "../../../Loader";
 import { CSSTransition } from "react-transition-group";
+import { FiPlus, FiSearch, FiEdit3, FiTrash2 } from "react-icons/fi";
 
 const ZaalControl = () => {
   const [type, setType] = useState({
@@ -44,25 +44,28 @@ const ZaalControl = () => {
 
   return (
     <div className={styles.zaalControlContainer}>
-      <div className={styles.buttonContainer}>
-        <CSSTransition in={type.posted} timeout={100} classNames="btn">
+      <div className={styles.zaalHeadContainer}>
+        <div className={styles.zaalSearchContainer}>
+          <input className={styles.zaalSearch} placeholder='Хайх...'></input>
+          <button className={styles.button}><FiSearch /></button>
+        </div>
+        <div className={styles.zaalButtonContainer}>
+          <CSSTransition in={type.posted} timeout={100} classNames="btn">
             <div className={styles.zaalButton} onClick={typePosted}>
               Posted
             </div>
-        </CSSTransition>
-        <CSSTransition in={type.saved} timeout={100} classNames="btn">
+          </CSSTransition>
+          <CSSTransition in={type.saved} timeout={100} classNames="btn">
             <div className={styles.zaalButton} onClick={typeSaved}>
               Deleted
             </div>
-        </CSSTransition>
-        <CSSTransition in={type.deleted} timeout={100} classNames="btn">
+          </CSSTransition>
+          <CSSTransition in={type.deleted} timeout={100} classNames="btn">
             <div className={styles.zaalButton} onClick={typeDeleted}>
               Saved
             </div>
-        </CSSTransition>
-      </div>
-      <div className={styles.search}>
-        <SidebarSearch />
+          </CSSTransition>
+        </div>
       </div>
       <div className={styles.sporthalls}>
         <ul className={styles.zaalContainer}>
@@ -70,12 +73,18 @@ const ZaalControl = () => {
             zaal.sporthalls.map((item) => (
               <li className={styles.zaal} key={item.hallId}>
                 <p className={styles.zaalTitle}>{item.title}</p>
-                <div className={styles.group}></div>
+                <div className={styles.group}>
+                  <button className={styles.button}><FiEdit3 /></button>
+                  <button className={styles.button}><FiTrash2 /></button>
+                </div>
               </li>
             ))}
           {zaal.loading && <Loader />}
         </ul>
       </div>
+      <button className={styles.addZaalButton}>
+        <FiPlus className={styles.icon} />
+      </button>
     </div>
   );
 };
